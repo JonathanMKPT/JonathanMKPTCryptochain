c6a8cf22fe9eb9a366bd6185d05a9e4f249fe0d6
JonathanMKPT <jonathan.thomas251@gmail.com>
2020-02-11 04:05:53 -0500

    Hardcoded values for genesis block and exported as object for rest of the app to use

---
+++ config.js
@@ -0,0 +1,8 @@
+//SCREEN CASE CODED, this is used to store dummmy values in first block
+const GENESIS_DATA = {
+    timestamp: 1,
+    lastHash : '-----',
+    hash :'hash-one',
+    data: []
+};
+module.exports = { GENESIS_DATA};
