adcb2cb4b767be15801a40540deea8458dac1f9c
JonathanMKPT <jonathan.thomas251@gmail.com>
2020-02-11 04:25:02 -0500

    Imported genesis block object before Block class

--- block.js
+++ block.js
@@ -1,3 +1,6 @@
+//inports data from the config.js file
+const { GENESIS_DATA } = require("./config");
+
 class Block{
      constructor({timestamp, lastHash, hash, data}){
          this.timestamp = timestamp;
@@ -5,6 +8,9 @@ class Block{
          this.hash = hash;
          this.data = data;
     }
+    //calls instance of genesis block from config.js
+    static genesis() {
+        return new this(GENESIS_DATA);
+    }
 }
-
 module.exports = Block;
