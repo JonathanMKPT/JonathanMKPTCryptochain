08d9f8238f407856b27d2ebe3c81c40d3bf2c6f0
JonathanMKPT <jonathan.thomas251@gmail.com>
2020-02-25 07:09:08 -0500

    Completed average work script

--- average-work.js
+++ average-work.js
@@ -12,6 +12,9 @@ for(let i=0; i<10000; i++){
     prevTimestamp = blockchain.chain[blockchain.chain.length-1].timestamp;
 
     blockchain.addBlock({ data: `block ${i}`});
+
+   
+
     nextBlock = blockchain.chain[blockchain.chain.length-1];
 
     nextTimestamp = nextBlock.timestamp;
