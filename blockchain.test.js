5a6bdbd3542355ddd54735575e87d818410f488a
JonathanMKPT <jonathan.thomas251@gmail.com>
2020-02-27 07:09:24 -0500

    Finalized block chain jump difficulty tests and method!

--- blockchain.test.js
+++ blockchain.test.js
@@ -1,5 +1,6 @@
 const Blockchain = require('./blockchain');
 const Block = require('./block');
+const cryptoHash = require('./crypto-hash');
 
 describe('Blockchain', () => {
     let blockchain, newChain, originalChain; 
@@ -54,21 +55,42 @@ describe('Blockchain', () => {
                         expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
                     });
                 });
+
+                describe('and the chain contains a block with an invalid field', () => {
+                    it('returns false', () => {
+                
+                        blockchain.chain[2].data = 'some-bad-and-evil-data';
+
+                        expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+                    });
+                });
+
+                describe('and the chain contains a block with a jumped difficulty', () => {
+                    it('return false', () => {
+                        const lastBlock = blockchain.chain[blockchain.chain.length-1];
+                        const lastHash = lastBlock.hash;
+                        const timestamp = Date.now();
+                        const nonce = 0;
+                        const data = [];
+                        const difficulty = lastBlock.difficulty - 3;
+                        const hash = cryptoHash(timestamp, lastHash, difficulty, nonce, data);
+                        const badBlock = new Block({
+                            timestamp, lastHash, hash, nonce, data
+                            
+                        });
+                        blockchain.chain.push(badBlock);
+
+                        expect(Blockchain.isValidChain(blockchain.chain).toBe(false));
+                    });
+                });
+
                 
                 describe('when the chain starts with the genesis block and has multiple blocks', () => {
                     describe('and a lastHash reference has changed', () => {
                         it('returns false', () => {});
                     });
 
-                    describe('and the chain contains a block with an invalid field', () => {
-                        it('returns false', () => {
-                    
-                            blockchain.chain[2].data = 'some-bad-and-evil-data';
-
-                            expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
-                        });
-                    });
-
+                 
                     describe('and the chain does not contain any invalid blocks', () => {
                         it('returns true', () => {
   
