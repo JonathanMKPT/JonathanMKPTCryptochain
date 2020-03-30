a7d4344743dd832ceb15bd52f1f3d3dd438ec637
JonathanMKPT <jonathan.thomas251@gmail.com>
2020-03-30 04:43:52 -0400

    Added get and listen methods for app

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const Blockchain = require('./blockchain');
 const app = express();
 
 const blockchain = new blockchain();
+
+app.get('/api/blocks', (req, res) => {
+    req.json(blockchain.chain);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`listening on localhost:${PORT}`));
